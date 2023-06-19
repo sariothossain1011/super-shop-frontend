1a6fbddb6d812a10c566cc86133cd4bc0a5bb43b
Sariot Hossain <106438276+sariothossain1011@users.noreply.github.com>
2023-06-19 22:27:02 +0600

    add base url

--- src/helper/config.js
+++ src/helper/config.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://super-shop-backend.vercel.app/api/v1"
-export const BASE_URL = "http://localhost:8080/api/v1"
+export const BASE_URL = "https://super-shop-backend.vercel.app/api/v1"
+// export const BASE_URL = "http://localhost:8080/api/v1"
